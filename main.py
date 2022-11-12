e3688ee6e3ad34469294dffa19fee22ce9afd7e8
kieha0809 <kieranha22@gmail.com>
2022-11-12 17:54:54 +0000

    Detect all 9 squares (static image)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-
-def hsv_to_colour(hsv):  # Function for converting HSV values to colour
+squares = []
+def hsv_to_colourTRIAL(hsv):  # Function for converting HSV values to colour
     colours = {  # HSV colour ranges
         'white': ([0, 0, 179], [179, 16, 255]),
         'yellow': ([15, 51, 0], [27, 183, 215]),
@@ -22,38 +22,43 @@ def detect_square(img_location):
     kernel = cv.getStructuringElement(cv.MORPH_RECT, (9, 9))
     dilated_frame = cv.dilate(canny_frame, kernel)
 
-    ret, thresh = cv.threshold(dilated_frame, 200, 255, 0)  # Thresholds the image
+    thresh = cv.adaptiveThreshold(dilated_frame, 255,
+                                  cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 21, 4)  # Thresholds the image
     cv.imshow('thresh', thresh)
-    contours, hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)  # Finds contours
-    min_area = 10000  # Defines the minimum area for a contour to be valid
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)  # Finds contours
+    min_area = 1000  # Defines the minimum area for a contour to be valid
+    max_area = 5000
     for contour in contours:  # Loops through all contours detected
         approx = cv.approxPolyDP(contour, 0.1 * cv.arcLength(contour, True),
                                  True)  # Gets the number of sides in each contour
         area = cv.contourArea(contour)  # Gets the area of each contour
-        if area > min_area and len(
-                approx) == 4:  # Checks if the contour is large enough to be the square and if it has 4 sides
+        if min_area < area < max_area and len(approx) == 4:  # Checks if number of sides and area are reasonable
             x, y, w, h = cv.boundingRect(contour)  # Gets coordinates of the corners of the square
-            cv.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)  # Draws the square onto the image
+            aspect_ratio = w / h  # Aspect ratio for a square should be one
+            if 0.8 < aspect_ratio < 1.2:
+                cv.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)  # Draws the square onto the image
+                squares.append([x+w,y+h]) #Appends coordiates of centre of square to array
 
     # cv.drawContours(img, contours, -1, (0, 255, 0), 3)  # Draws on the contours
-
+    print(squares)
+    print(squares.sort())
     cv.imshow('img', img)
     cv.waitKey(0)
     return x, y, w, h
 
 
 def get_square_coordinates(x, y, w, h):
-    coordinates = [] #Coordinates of the centres of each square
-    increment = w / 3 # The increment that is added to the x coordinate to get between squares at the same height
-    for i in range(3): #Loops through all squares on the top row and gets their coordinates
+    coordinates = []  # Coordinates of the centres of each square
+    increment = w / 3  # The increment that is added to the x coordinate to get between squares at the same height
+    for i in range(3):  # Loops through all squares on the top row and gets their coordinates
         x_cor = x + x / 6 + i * increment
         y_cor = y + y / 6
         coordinates.append((x_cor, y_cor))
-    for i in range(3): #Repeats the above procedure for the second row
+    for i in range(3):  # Repeats the above procedure for the second row
         x_cor = x + x / 6 + i * increment
         y_cor = y + y / 2
         coordinates.append((x_cor, y_cor))
-    for i in range(3): #Repeats for the third row
+    for i in range(3):  # Repeats for the third row
         x_cor = x + x / 6 + i * increment
         y_cor = y + 5 * y / 6
         coordinates.append((x_cor, y_cor))
@@ -61,10 +66,26 @@ def get_square_coordinates(x, y, w, h):
     return coordinates
 
 
-#detect_square('cube.jpg')
-'''img = cv.imread('cube.jpg')
-cv.imshow('img',img)
-cv.waitKey(0)'''
+def hsv_to_colour(h, s, v):
+    if h < 5 and s > 5:
+        return 'r'
+    elif h < 10 and h >= 3:
+        return 'o'
+    elif h <= 25 and h > 10:
+        return 'y'
+    elif h >= 70 and h <= 85 and s > 100 and v < 180:
+        return 'g'
+    elif h <= 130 and s > 70:
+        return 'b'
+    elif h <= 100 and s < 10 and v < 200:
+        return 'w'
+    else:
+        return 'w'
 
-a=get_square_coordinates(10,10,2,2)
-print(a)
+
+#def get_hsv([x,y]):
+    #rgb =
+detect_square('cube4.jpg')
+
+# a=get_square_coordinates(10,10,2,2)
+# print(a)
