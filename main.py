4dd65a1c7c85381045624f0c0b46104764a1b923
kieha0809 <kieranha22@gmail.com>
2022-11-14 21:43:31 +0000

    Detects squares through webcam feed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import numpy as np
 
 squares = []
+
+
 def hsv_to_colourTRIAL(hsv):  # Function for converting HSV values to colour
     colours = {  # HSV colour ranges
         'white': ([0, 0, 179], [179, 16, 255]),
@@ -13,21 +15,23 @@ def hsv_to_colourTRIAL(hsv):  # Function for converting HSV values to colour
     }
 
 
-def detect_square(img_location):
-    img = cv.imread(img_location)  # Reads in an image of the cube face
-    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Converts the image to grayscale
+def detect_square(frame):
+    # img = cv.imread(frame)  # Reads in an image of the cube face
+    gray_img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  # Converts the image to grayscale
 
-    blurred_frame = cv.blur(gray_img, (3, 3))  # Performs morphological operations: blurring,kernelling and dilation
+    blurred_frame = cv.medianBlur(gray_img, 3)  # Performs morphological operations: blurring,kernelling and dilation
+    #sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    #sharpen = cv.filter2D(blurred_frame, -1, sharpen_kernel)
     canny_frame = cv.Canny(blurred_frame, 30, 60, 3)
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (9, 9))
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))
     dilated_frame = cv.dilate(canny_frame, kernel)
+    close = cv.morphologyEx(dilated_frame, cv.MORPH_CLOSE, kernel, iterations = 2)
 
-    thresh = cv.adaptiveThreshold(dilated_frame, 255,
-                                  cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 21, 4)  # Thresholds the image
+    thresh = cv.adaptiveThreshold(close, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY_INV,21,4)  # Thresholds the image
     cv.imshow('thresh', thresh)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)  # Finds contours
     min_area = 1000  # Defines the minimum area for a contour to be valid
-    max_area = 5000
+    max_area = 2000
     for contour in contours:  # Loops through all contours detected
         approx = cv.approxPolyDP(contour, 0.1 * cv.arcLength(contour, True),
                                  True)  # Gets the number of sides in each contour
@@ -35,16 +39,16 @@ def detect_square(img_location):
         if min_area < area < max_area and len(approx) == 4:  # Checks if number of sides and area are reasonable
             x, y, w, h = cv.boundingRect(contour)  # Gets coordinates of the corners of the square
             aspect_ratio = w / h  # Aspect ratio for a square should be one
-            if 0.8 < aspect_ratio < 1.2:
-                cv.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)  # Draws the square onto the image
-                squares.append([x+w,y+h]) #Appends coordiates of centre of square to array
+            if 0.9 < aspect_ratio < 1.1:  # Checks if aspect ratio is close enough to 1 to be classified as a square
+                cv.rectangle(frame, (x, y), (x + w, y + h), (36, 255, 12), 2)  # Draws the square onto the image
+                # squares.append([x+w,y+h]) #Appends coordinates of centre of square to array
 
     # cv.drawContours(img, contours, -1, (0, 255, 0), 3)  # Draws on the contours
-    print(squares)
-    print(squares.sort())
-    cv.imshow('img', img)
-    cv.waitKey(0)
-    return x, y, w, h
+    # print(squares)
+    # print(squares.sort())
+    cv.imshow('img', frame)
+    cv.waitKey(1)
+    # return x, y, w, h
 
 
 def get_square_coordinates(x, y, w, h):
@@ -83,9 +87,14 @@ def hsv_to_colour(h, s, v):
         return 'w'
 
 
-#def get_hsv([x,y]):
-    #rgb =
-detect_square('cube4.jpg')
+vid = cv.VideoCapture(0)
+while True:
+    ret, frame = vid.read()
+    detect_square(frame)
+
+# def get_hsv([x,y]):
+# rgb =
+detect_square('cube2.jpg')
 
 # a=get_square_coordinates(10,10,2,2)
 # print(a)
